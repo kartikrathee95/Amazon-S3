76fa2105687bedcfc4e2e3cc81f2fe85d6408259
kartikrathee95 <96821019+kartikrathee95@users.noreply.github.com>
2024-11-11 09:22:12 +0530

    SECREY KEY- ENV

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,7 +30,7 @@ create_database_if_not_exists()
 create_tables()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = "r3K6sbv0e9F6JpxtsRmtV9f3XtU88e9TwL6zMbLQF3w6z5-8wUcmk9JX1A8Lv2pE"
+SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = "HS256"
 TOKEN_URL = "/S3/auth/oauth/login"
 
