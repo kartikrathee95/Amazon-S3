e7ace8999cd1a78331fd7fec316af94ae00f564a
kartikrathee95 <96821019+kartikrathee95@users.noreply.github.com>
2024-11-11 09:50:13 +0530

    Refresh Select File

--- frontend/src/components/FileManagement/FileUpload.js
+++ frontend/src/components/FileManagement/FileUpload.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { uploadFile } from '../../api';
 import { useUser } from '../UserContext';
+
 const FileUpload = ({ onUploadSuccess }) => {
     const [file, setFile] = useState(null);
     const [folderName, setFolderName] = useState('');
-    const [originalFileName, setOriginalFileName] = useState('');  // State to store the original file name
-    const {username} = useUser();
+    const [originalFileName, setOriginalFileName] = useState('');
+    const { username } = useUser();
+    const fileInputRef = useRef(null);
+
     const handleUpload = async (e) => {
         e.preventDefault();
+
         if (!file) {
             console.error('No file selected');
             alert('Please select a file to upload');
@@ -20,10 +24,14 @@ const FileUpload = ({ onUploadSuccess }) => {
             console.log('File uploaded successfully');
             alert('File uploaded successfully');
 
-            // Set the original filename after successful upload (from response)
-            setOriginalFileName(response.original_filename || file.name);  // Handle versioned filename
+            setOriginalFileName(response.original_filename || file.name);
 
-            onUploadSuccess(); // Refresh the file list on success
+            onUploadSuccess();  // Refresh the file list on success
+
+            setFile(null);
+            setFolderName('');
+            fileInputRef.current.value = '';
+
         } catch (error) {
             console.error('File upload failed:', error.response ? error.response.data : error);
         }
@@ -33,15 +41,16 @@ const FileUpload = ({ onUploadSuccess }) => {
         <div>
             <h2>Upload File</h2>
             <form onSubmit={handleUpload}>
-                <input 
-                    type="file" 
-                    onChange={(e) => setFile(e.target.files[0])} 
+                <input
+                    type="file"
+                    ref={fileInputRef}
+                    onChange={(e) => setFile(e.target.files[0])}
                 />
-                <input 
-                    type="text" 
-                    value={folderName} 
-                    onChange={(e) => setFolderName(e.target.value)} 
-                    placeholder="Folder Name (optional)" 
+                <input
+                    type="text"
+                    value={folderName}
+                    onChange={(e) => setFolderName(e.target.value)}
+                    placeholder="Folder Name (optional)"
                 />
                 <button type="submit">Upload</button>
             </form>
